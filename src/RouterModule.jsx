583ac14124721db37196ca1ea62b6ec2a5072c2d
Shubhjn4357 <shubhamjain.com.in@gmail.com>
2022-09-14 18:12:32 +0530

    debug

--- src/RouterModule.jsx
+++ src/RouterModule.jsx
@@ -4,11 +4,13 @@ import Login from "./auth/Login";
 import SignUp from "./auth/SignUp";
 import Dashboard from "./component/Dashboard";
 import Forms from "./component/Forms";
+import Verify from "./component/verify";
 import Preview from "./component/Preview";
 const RouterModule=()=>{
   return (<>
   <Routes>
     <Route exact path="/" element={<Category/>}/>
+    <Route exact path="/verify" element={<Verify/>}/>
     <Route exact path="/dashboard/teacher" element={<Dashboard/>}/>
     <Route exact path="/form/student/:id" element={<Forms/>}/>
     <Route exact path="/preview/:link" element={<Preview/>}/>
